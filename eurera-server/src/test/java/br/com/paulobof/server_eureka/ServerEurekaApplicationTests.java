964707884964c3dcdc61a3c742367f2476523e1f
paulobof <paulobof@gmail.com>
2024-11-16 18:42:27 -0300

    refactor - Correção Pastas Geral

---
+++ eurera-server/src/test/java/br/com/paulobof/server_eureka/ServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulobof.server_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
