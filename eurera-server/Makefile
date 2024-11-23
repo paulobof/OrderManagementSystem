0bf610bf0b8bc59ecfbe27593d9c8044ebec4d2f
paulobof <paulobof@gmail.com>
2024-11-23 12:48:01 -0300

    fix - Correção Eureka Server

---
+++ eurera-server/Makefile
@@ -0,0 +1,8 @@
+build:
+	mvn complile
+
+package:
+	mvn package
+
+docker-build:
+	docker build -t customer-management:1.0 .
--- eurera-server/src/test/java/br/com/paulobof/servereureka/ServerEurekaApplicationTests.java
+++ eurera-server/src/test/java/br/com/paulobof/servereureka/ServerEurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.paulobof.server_eureka;
+package br.com.paulobof.servereureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
